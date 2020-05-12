ee0e273b40fe62c046658893505c44123ae65ef4
Unknown <mlund@itba.edu.ar>
2020-05-12 16:10:09 -0300

    Added babel-plugin-react-css-modules

---
+++ src/containers/Test.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './Test.css';
+
+class Test extends Component {
+    render () {
+        return (
+            <div>
+                <p styleName='test'>Chau</p>
+            </div>
+        );
+    }
+}
+
+export default Test;
